82b4d6bd618395c9bec707739828144a22393ced
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-11-10 13:14:46 -0800

    Users can now successfully live stream using OBS

--- client/src/components/streams/ViewStream.js
+++ client/src/components/streams/ViewStream.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { getStream } from '../../actions';
 
 class ViewStream extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.getStream(this.props.match.params.id);
+    this.constructPlayer();
+  }
+
+  componentDidUpdate() {
+    this.constructPlayer();
+  }
+
+  componentWillUnmount() {
+    // console.log('UNMOUNTED');
+    this.player.destroy();
+  }
+
+  constructPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
-    return <div>ViewStream</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
   }
 }
 
-export default connect(null, { getStream })(ViewStream);
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { getStream })(ViewStream);
